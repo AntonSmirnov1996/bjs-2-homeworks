156c9891459eee30cfbd27f0a3f6d7931c2e40b6
Anton Smirnov <samx2x2@gmail.com>
2023-04-19 15:16:39 +0300

    first task commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,15 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  
   let arr = [];
-  
+  const d = Math.pow(b, 2) - 4 * a * c;
+  console.log(d);
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / ( 2 * a )];
+  } else {
+    arr = [(-b + Math.sqrt(d) ) / (2*a), (-b - Math.sqrt(d) ) / (2*a)];
+  }
   return arr;
 }
 
